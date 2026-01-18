4b1303ac816dcaa49ba68f87c29419e079be147f
ledangkhanhdut <khanhdangabc2@gmail.com>
2026-01-18 09:12:00 +0700

    feat(ui): create header component with user avatar

---
+++ admin-dashboard/src/layout/header.tsx
@@ -0,0 +1,23 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import MobileSidebar from "./mobile-sidebar";
+
+export default function Header() {
+  return (
+    <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6 sticky top-0 z-50 bg-background">
+      {/* Mobile Sidebar Trigger */}
+      <MobileSidebar />
+
+      <div className="w-full flex-1">
+        {/* Chỗ này để thanh Search sau này */}
+      </div>
+
+      <Button variant="ghost" size="icon" className="rounded-full">
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </Button>
+    </header>
+  );
+}
